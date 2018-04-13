857e9b62fd549f950f0e045ae9bf80025f755dfe
Iain Soars <iain.soars@nice.com>
2018-04-13 16:42:46 +0100

    Fixing unit tests

--- src/app/testing/time.service.mock.ts
+++ src/app/testing/time.service.mock.ts
@@ -1,3 +1,16 @@
+import { Watch } from '../domain/watch';
+
 export class MockTimeService {
 
+    public getWatches(): Watch[] {
+        const watches = [
+            new Watch('First watch', 20, 0),
+        ];
+
+        return watches;
+    }
+
+    public getShipsTime(time: Date): string {
+        return 'One bell in the First watch';
+    }
 }
