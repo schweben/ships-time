e6329b4fecc5bd4a510120ddab2f4be458d511d8
Iain Soars <iain.soars@bca.com>
2024-01-11 21:17:24 +0000

    Converting to React

---
+++ src/app/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Top Trumps</h1>
+      <img src="images/"
+      <Setup startGame={startGame}/>
+    </div>
+  );
+}
+
+export default App;
