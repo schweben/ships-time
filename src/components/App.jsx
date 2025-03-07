4dc07f3ba9fb1ec06654fb9980f21a86d5934c21
Iain Soars <iain.soars@bca.com>
2025-03-07 11:48:12 +0000

    Changed background image

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,9 +10,6 @@ function App() {
       <h1>Ship's Time</h1>
       <CurrentWatch/>
       <Watches/>
-      <div className="imageAttribution">
-        <a href="https://www.freepik.com/free-vector/realistic-anchor-with-rope_9462235.htm#query=anchor&position=10&from_view=keyword&track=sph&uuid=30d7bf2e-0859-48a9-9b24-eb5d2a576375">Image by macrovector</a> on Freepik
-      </div>
     </div>
   );
 }
